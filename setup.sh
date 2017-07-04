5746998a587f9156321be359af8faf5094a8eca0
Steffen Elste <steffen.elste@gmail.com>
2017-07-04 14:55:04 +0200

    Fix - no rebuild on startup

--- setup.sh
+++ setup.sh
@@ -8,4 +8,4 @@ DOCKER_GROUP_ID=`getent group docker | cut -d: -f3`
 echo "DOCKER_GROUP_ID=${DOCKER_GROUP_ID}" > .env
 
 echo "Startup Docker Compose"
-docker-compose -f docker-compose.yml up -d --build
+docker-compose -f docker-compose.yml up -d
