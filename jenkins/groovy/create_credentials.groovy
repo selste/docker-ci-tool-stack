7694adbb85614367f7d7ee213f3f809c91b6f8d7
Steffen Elste <steffen.elste@gmail.com>
2017-07-07 12:15:06 +0200

    Fix - setup credential for accessing Nexus programmatically on Jenkins startup

---
+++ jenkins/groovy/create_credentials.groovy
@@ -0,0 +1,16 @@
+import jenkins.model.*
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.common.*
+import com.cloudbees.plugins.credentials.impl.*
+
+def list = CredentialsProvider.lookupCredentials(StandardUsernameCredentials.class, Jenkins.instance, null, null)
+if (null == list.findResult { it.id.equalsIgnoreCase('nexus') ? it:null }) {
+  println 'Result list is empty'
+
+  SystemCredentialsProvider.getInstance().getCredentials()
+    .add(new UsernamePasswordCredentialsImpl(CredentialsScope.GLOBAL, "nexus", "Nexus Admin User", "admin", "admin123"))
+
+  println 'Credentials successfully created'
+} else {
+  println "Credentials for id 'nexus' already exist"
+}
