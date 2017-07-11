b3357c022487dbb303b8ceb377847946663616eb
Steffen Elste <steffen.elste@gmail.com>
2017-07-11 08:25:20 +0200

    Cleanup

--- nexus/scripts/repositories.sh
+++ nexus/scripts/repositories.sh
@@ -8,13 +8,16 @@ scripts="nuget.json docker.json"
 # Script aliases as used by Nexus
 aliases="nuget docker"
 
+# Repository Proxy URL
+url='http://localhost:18081/service/siesta/rest/v1/script'
+
 # Uploading
 printf "Processing scripts $scripts ...\n\n"
 
 for script in $scripts
 do
   printf "Creating Integration API Script from $script"
-  status=$(curl -s -o /dev/null -w '%{http_code}' -u admin:admin123 --header 'Content-Type: application/json' 'http://localhost:18081/service/siesta/rest/v1/script/' -d @$script)
+  status=$(curl -s -o /dev/null -w '%{http_code}' -u admin:admin123 --header 'Content-Type: application/json' $url -d @$script)
 
   if [ $status -eq 204 ]; then
     echo " ... done"
@@ -31,7 +34,7 @@ printf "Running aliases $aliases ...\n\n"
 for alias in $aliases
 do
   printf "Running alias $alias"
-  status=$(curl -s -o /dev/null -w '%{http_code}' -X POST -u admin:admin123 --header 'Content-Type: text/plain' "http://localhost:18081/service/siesta/rest/v1/script/$alias/run")
+  status=$(curl -s -o /dev/null -w '%{http_code}' -X POST -u admin:admin123 --header 'Content-Type: text/plain' "$url/$alias/run")
 
   if [ $status -eq 200 ]; then
     echo " ... done"
